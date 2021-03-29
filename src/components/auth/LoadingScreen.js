93cb3556c8695ed42553849e9ee426884305cf5a
Bernhard Silva <bernhard.silva@gmail.com>
2021-03-28 22:48:33 -0300

    alt added to image of Loading on LoadingScreen

--- src/components/auth/LoadingScreen.js
+++ src/components/auth/LoadingScreen.js
@@ -5,7 +5,7 @@ export const LoadingScreen = () => {
   return (
     <div className="loading__main">
       <h2 className="loading__title">Please wait...</h2>
-      <img src={Loading} className="loading__img"></img>
+      <img src={Loading} className="loading__img" alt="Loading" />
     </div>
   );
 };
