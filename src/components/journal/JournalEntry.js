b6eb75e6e445d0dd879a7eaed6ccc6548e4658ba
Bernhard Silva <bernhard.silva@gmail.com>
2021-04-09 10:45:36 -0400

    take values from active note and put the value in noteScreen in redux storage

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import moment from 'moment';
+import { useDispatch } from 'react-redux';
+import { activeNote } from '../../actions/notes';
 
 export const JournalEntry = ({ id, date, title, body, url }) => {
   const noteDate = moment(date);
+  const dispatch = useDispatch();
 
-  const handleJournalEntry = () => {
-    alert('Journal entry');
+  const handleEntryClick = () => {
+    dispatch(
+      activeNote(id, {
+        date,
+        title,
+        body,
+        url,
+      }),
+    );
   };
 
   return (
-    <div className="journal__entry" onClick={handleJournalEntry}>
+    <div className="journal__entry" onClick={handleEntryClick}>
       {url && ( //si el url existe se muestra, de lo contrario, no.
         <div
           className="journal__entry-picture"
--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { useForm } from '../../hooks/useForm';
 import { NotesAppBar } from './NotesAppBar';
 
 export const NoteScreen = () => {
+  const { active: note } = useSelector((state) => state.notes);
+  const [formValues, handleInputChange] = useForm(note);
+  console.log(formValues);
+  const { body, title, url } = formValues;
+
   return (
     <div className="notes__main-content">
       <NotesAppBar />
@@ -12,19 +19,25 @@ export const NoteScreen = () => {
           placeholder="Write here your title"
           className="notes__title-input"
           autoComplete="off"
+          value={title}
+          onChange={handleInputChange}
         />
 
         <textarea
           placeholder="Write here your event 📝"
           className="notes__textarea"
+          value={body}
+          onChange={handleInputChange}
         ></textarea>
 
-        <div className="notes__image">
-          <img
-            src="http://www.taiwaneseamerican.org/wp-content/uploads/mree_kristina-450x304.jpg"
-            alt="note-img"
-          />
-        </div>
+        {url && (
+          <div className="notes__image">
+            <img
+              src="http://www.taiwaneseamerican.org/wp-content/uploads/mree_kristina-450x304.jpg"
+              alt="note-img"
+            />
+          </div>
+        )}
       </div>
     </div>
   );
